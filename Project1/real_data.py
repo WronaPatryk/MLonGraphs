4ca59dcffb3d8471653d7ad98b058bb66b6f11d1
soffi49 <zosia.wrona49@gmail.com>
2023-04-01 20:29:08 +0200

    [ML] Reading data into adjacency matrix

--- Project1/real_data.py
+++ Project1/real_data.py
@@ -1,19 +1,33 @@
+import os
+import numpy as np
+
 # directory name storing graph data
 DIR_NAME = 'graph-data'
 
+# Function converts edge list into adjacency matrix
+#
+# data - edge list
+def getAdjacencyFromCSV(data):
+    vertexNo = np.max(data) + 1
+    adjacencyMatrix = np.zeros((vertexNo, vertexNo), dtype=np.int64)
+    
+    for x in data:
+        adjacencyMatrix[x[0]][x[1]] = 1
 
-# helper function used to read CSV file
+    return adjacencyMatrix
+    
+
+# Function reads CSV file
+#
 # filename - name of the csv to be read
 def readDataFromCSV(filename):
-    import os
-    import numpy as np
-
     file_absolute_path = os.path.dirname(__file__)
     file_path = os.path.join(file_absolute_path, DIR_NAME, filename)
 
     with open(file_path) as file:
         lines = (line for line in file if not line.startswith("#"))
-        return np.loadtxt(lines, delimiter=',', skiprows=1)
+        data = np.loadtxt(lines, delimiter=',', skiprows=1, dtype=np.int64)
+        return getAdjacencyFromCSV(data)
 
 
 # data set information: Nodes: 50,515  Edges:  819,306
