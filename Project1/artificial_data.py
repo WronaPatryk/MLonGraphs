096f9dc958b430c79a0402cbbacef72522d8cdd5
soffi49 <zosia.wrona49@gmail.com>
2023-04-02 23:48:09 +0200

    Refactored artificial_data import

--- Project1/artificial_data.py
+++ Project1/artificial_data.py
@@ -1,26 +1,29 @@
+import sys, os
+import numpy as np
+import clustbench
 
+# Funcion impots data based on given parameters
+#
+# catalog - name of the catalog
+# set - name of the set
+# returns: ndarrays: data, labels
+def importData(catalog, set):
+    sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
 
+    clustbenchResult = clustbench.load_dataset(catalog, set, "clustering-data-v1")    
+    return clustbenchResult[3], clustbenchResult[4] 
+
+# Funtion returns the largest artificial data set
+def importLargeArtificialData():
+    # 2 dimensions, 1500 points
+    return importData("sipu", "birch1")
+
+# Funtion returns the medium artificial data set
 def importBiggerArtificialData():
     # 3 dimensions, 1500 points
+    return importData("wut", "mk4")
     
-    import sys, os
-    import numpy as np
-    sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
-    import clustbench
-    
-    clustbenchResult = clustbench.load_dataset("wut", "mk4", "clustering-data-v1")
-    
-    return clustbenchResult[3], clustbenchResult[4] # ndarrays: data, labels
-    
-    
+# Funtion returns the smallest artificial data set
 def importSmallerArtificialData():
     # 2 dimensions, 192 points
-    
-    import sys, os
-    import numpy as np
-    sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
-    import clustbench
-    
-    clustbenchResult = clustbench.load_dataset("wut", "z1", "clustering-data-v1")
-
-    return clustbenchResult[3], clustbenchResult[4] # ndarrays: data, labels
+    return importData("wut", "z1")
